dd618d6e236abbf3722d369d0b80a80f2b433569
Takos22 <takos2210@gmail.com>
2021-02-05 21:00:12 +0100

    ✨ Add Grid.from_json class method

--- maze_generator/grid.py
+++ maze_generator/grid.py
@@ -1,4 +1,4 @@
-from json import dump
+from json import dump, load
 from typing import Iterator, List, Optional, Tuple
 
 from .cell import Cell
@@ -57,6 +57,10 @@ class Grid:
             + "#" * (self.width + 2)
         )
 
+    def reset(self):
+        for cell in self:
+            cell.reset()
+
     def to_json(self, filename: Optional[str] = None) -> List[List[int]]:
         json_grid = []
 
@@ -76,6 +80,34 @@ class Grid:
 
         return json_grid
 
-    def reset(self):
-        for cell in self:
-            cell.reset()
+    @classmethod
+    def from_json(cls, filename: str = "grid.json") -> "Grid":
+        with open(filename) as f:
+            json_grid = load(f)
+
+        self: Grid = cls.__new__(cls)
+        self.width = len(json_grid[0])
+        self.height = len(json_grid)
+        self._grid = []
+
+        for y, json_line in enumerate(json_grid):
+            line = []
+
+            for x, json_cell in enumerate(json_line):
+                cell = Cell(x, y, obstacle=(json_cell == 1))
+                line.append(cell)
+
+                if json_cell == 2:
+                    self.start = cell
+                    cell.start = True
+                    cell.distance_from_start = 0
+                elif json_cell == 3:
+                    self.end = cell
+                    cell.end = True
+
+            self._grid.append(line)
+
+        # invert lines and columns
+        self._grid = [list(column) for column in zip(*self._grid)]
+
+        return self
